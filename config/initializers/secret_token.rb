2c400cf7789e049ac7bfb7a79b9dfd32c1b4f0ef
suren reddy <suren@simplecue.com>
2012-11-14 19:13:00 +1000

    init proj

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pingly::Application.config.secret_token = '2f328f42534fddbe7d5d411f0d9c7c5d5f771b573c108745a5332bdb9626c53dbb6f02cf39c7cc5bff75e269ce09bdf7634c5007920d3c4fb0bde8cb100b7ccf'
